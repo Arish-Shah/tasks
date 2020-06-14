e2440e7da449c0686228f7c8499367fc7cc17c0f
Arish-Shah <arish0246@gmail.com>
2020-06-14 11:46:36 +0530

    add to toolbar

--- ckeditor/src/plugins/saved-templates.js
+++ ckeditor/src/plugins/saved-templates.js
@@ -1,39 +1,75 @@
+import { 
+  createDropdown, 
+  addListToDropdown
+} from '@ckeditor/ckeditor5-ui/src/dropdown/utils';
+import Collection from '@ckeditor/ckeditor5-utils/src/collection';
+import Model from '@ckeditor/ckeditor5-ui/src/model';
+
 class SavedTemplates {
   static init() {
-    // we can even fetch templates here
     this.templates = [];
-    this.render();
+    this.items = new Collection();
+    this.createPlugin();
+  }
+
+  static createPlugin() {
+    const toolbar = document.querySelector('.ck-toolbar__items');
+
+    for (const template of this.templates) {
+      this.addToDropdown(template);
+    }
+
+    this.dropdown = createDropdown(editor.locale);
+    this.dropdown.buttonView.set({
+      label: 'Templates',
+      withText: true
+    });
+    addListToDropdown(this.dropdown, this.items);
+    this.dropdown.render();
+    
+    const items = this.getChildNodes();
+    items.forEach((item, index) => {
+      this.hydrate(item, this.templates[index]);
+    });
+    
+    toolbar.appendChild(this.dropdown.element);
   }
 
   static add(template) {
     this.templates.push(template);
-    this.render();
+    this.addToDropdown(template);
+    this.hydrate(this.getAddedNode(), template);
   }
 
-  static render() {
-    this.root = document.querySelector('.saved-templates');
-    this.cleanNode(this.root);
-
-    for (const template of this.templates) {
-      const div = document.createElement('div');
-      div.classList = 'saved-template';
-      div.innerHTML = template;
-      div.addEventListener('click', this.handleClick);
-      this.root.appendChild(div);
-    }
+  static addToDropdown(template) {
+    this.items.add({ 
+      type: 'button', 
+      model: new Model({
+        withText: true,
+        label: template
+      }) 
+    });
   }
 
-  static handleClick() {
-    const html = this.innerHTML;
+  static hydrate(listItem, template) {
+    const span = listItem.querySelector('.ck-button__label');
+    span.innerHTML = template;
+    listItem.addEventListener('click', () => this.handleClick(template));
+  }
+
+  static handleClick(html) {
     const viewFragment = editor.data.processor.toView(html);
     const modelFragment = editor.data.toModel(viewFragment);
     editor.model.insertContent(modelFragment);
   }
 
-  static cleanNode(node) {
-    while (node.firstChild) {
-      node.removeChild(node.firstChild);
-    }
+  static getAddedNode() {
+    const childNodes = this.getChildNodes();
+    return childNodes[childNodes.length - 1]; 
+  }
+
+  static getChildNodes() {
+    return this.dropdown.listView.element.childNodes
   }
 }
 
